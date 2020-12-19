63bd216d7ac74d8ac3b692a6a3a0590e64dd3510
Casey Avila <casey@theavilas.org>
2020-12-19 15:37:16 -0800

    Initial DOM progress

--- School-Loop-Plugin.js
+++ School-Loop-Plugin.js
@@ -1,2 +1,10 @@
-document.body.style.border = "5px solid red";
+// document.body.style.border = "5px solid red";
+console.log("EXTENSION LOADED");
+
+var assignments = document.querySelectorAll("[data-track-container='Active Assignments']")[0];
+var assignmentArray = assignments.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
+
+assignments.getElementsByClassName("sub_header")[0].innerHTML = "HELLO";
+
+console.log(assignmentArray);
 
