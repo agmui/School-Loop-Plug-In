e04d1557f4a1c5cd4bf02a0b696496840a053b17
Casey Avila <casey@theavilas.org>
2020-12-19 17:57:48 -0800

    Clean up code

--- School-Loop-Plugin.js
+++ School-Loop-Plugin.js
@@ -1,53 +1,41 @@
-/*var assignmentContainer = document.querySelectorAll("[data-track-container='Active Assignments']")[0];
-var assignments = assignmentContainer.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
+function getAssignments() {
+    const assignmentContainer = document.querySelector("[data-track-container='Active Assignments']");
+    let assignments = assignmentContainer.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
 
-var table = assignments[0].getElementsByClassName("ajax_accordion_row jsTrackerRefresh")[0].getElementsByClassName("table_basic")[0]
-var x = table.getElementsByClassName("column padding_5")[3].innerText*/
-
-
-//assignmentContainer.getElementsByClassName("sub_header")[0].innerHTML = "HELLO";
+    return assignments;
+}
 
 function getSchoolClass(x) {
-  var assignmentContainer = document.querySelectorAll("[data-track-container='Active Assignments']")[0];
-  var assignments = assignmentContainer.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
-
-  var table = assignments[x].getElementsByClassName("ajax_accordion_row jsTrackerRefresh")[0].getElementsByClassName("table_basic")[0]
-  var schoolClass = table.getElementsByClassName("column padding_5")[3].innerText
-  var dueDate = table.getElementsByClassName("column padding_5")[4].innerText
-    return schoolClass +' '+ dueDate;
-  }
-
-function getNumberOfAssignments(){ //try to use superfunctions
-  var assignmentContainer = document.querySelectorAll("[data-track-container='Active Assignments']")[0];
-  var assignments = assignmentContainer.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
-  return assignments.length
+    var table = getAssignments()[x].getElementsByClassName("ajax_accordion_row jsTrackerRefresh")[0].getElementsByClassName("table_basic")[0]
+    var schoolClass = table.getElementsByClassName("column padding_5")[3].innerText
+    var dueDate = table.getElementsByClassName("column padding_5")[4].innerText
+    return schoolClass + ' ' + dueDate;
 }
 
 function reOrder() {
-  /*divOne = document.getElementById('#div1');
-  divTwo = document.getElementById('#div2');
-  divThree = document.getElementById('#div3');
-  container = divOne.parentNode;
-  container.appendChild(divTwo);
-  container.appendChild(divOne);
-  container.appendChild(divThree);*/
+    /*divOne = document.getElementById('#div1');
+    divTwo = document.getElementById('#div2');
+    divThree = document.getElementById('#div3');
+    container = divOne.parentNode;
+    container.appendChild(divTwo);
+    container.appendChild(divOne);
+    container.appendChild(divThree);*/
 
-  let help = []
-  for (let i = 0; i < getNumberOfAssignments(); i++) {
-    var assignmentContainer = document.querySelectorAll("[data-track-container='Active Assignments']")[0];
-    var assignments = assignmentContainer.getElementsByClassName("content")[0].getElementsByClassName("ajax_accordion");
-    help[i] = assignments[i];
-  }
-  container = help[0].parentNode;
-  for (let i = 0; i < getNumberOfAssignments(); i++){
-    container.appendChild(help[i]);
-  }
+    let help = []
+    for (i = 0; i < getAssignments().length; i++) {
+        help[i] = getAssignments()[i];
+    }
+
+    container = help[0].parentNode;
+    for (i = 0; i < getAssignments().length; i++){
+        container.appendChild(help[i]);
+    }
 }
-
 
 let myArray = []
-for (let i = 0; i < getNumberOfAssignments(); i++) {
-  myArray.push(getSchoolClass(i));
+for (i = 0; i < getAssignments().length; i++) {
+    myArray.push(getSchoolClass(i));
 }
+
 console.log(myArray.sort());
 reOrder()
